0ee7b7f0cda302f6a584194e74cc026d21f6f09a
Alkar-stud <alexandre.kapps@gmail.com>
2025-04-16 15:51:33 +0200

    Add Covoiturage and CovoiturageStatus entities with repositories and migration

---
+++ src/Entity/Covoiturage.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CovoiturageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CovoiturageRepository::class)]
+class Covoiturage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $startingAddress = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $arrivalAddress = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startingAt = null;
+
+    #[ORM\Column]
+    private ?int $tripDuration = null;
+
+    #[ORM\Column]
+    private ?int $nbCredit = null;
+
+    #[ORM\Column]
+    private ?int $nbPlace = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?CovoiturageStatus $status = null;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\ManyToMany(targetEntity: User::class)]
+    private Collection $participants;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartingAddress(): ?string
+    {
+        return $this->startingAddress;
+    }
+
+    public function setStartingAddress(string $startingAddress): static
+    {
+        $this->startingAddress = $startingAddress;
+
+        return $this;
+    }
+
+    public function getArrivalAddress(): ?string
+    {
+        return $this->arrivalAddress;
+    }
+
+    public function setArrivalAddress(string $arrivalAddress): static
+    {
+        $this->arrivalAddress = $arrivalAddress;
+
+        return $this;
+    }
+
+    public function getStartingAt(): ?\DateTimeImmutable
+    {
+        return $this->startingAt;
+    }
+
+    public function setStartingAt(\DateTimeImmutable $startingAt): static
+    {
+        $this->startingAt = $startingAt;
+
+        return $this;
+    }
+
+    public function getTripDuration(): ?int
+    {
+        return $this->tripDuration;
+    }
+
+    public function setTripDuration(int $tripDuration): static
+    {
+        $this->tripDuration = $tripDuration;
+
+        return $this;
+    }
+
+    public function getNbCredit(): ?int
+    {
+        return $this->nbCredit;
+    }
+
+    public function setNbCredit(int $nbCredit): static
+    {
+        $this->nbCredit = $nbCredit;
+
+        return $this;
+    }
+
+    public function getNbPlace(): ?int
+    {
+        return $this->nbPlace;
+    }
+
+    public function setNbPlace(int $nbPlace): static
+    {
+        $this->nbPlace = $nbPlace;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getStatus(): ?CovoiturageStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?CovoiturageStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(User $participant): static
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants->add($participant);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(User $participant): static
+    {
+        $this->participants->removeElement($participant);
+
+        return $this;
+    }
+}
