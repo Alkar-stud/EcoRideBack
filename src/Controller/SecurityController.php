9e82679c434fdd8a45ba1d9df5f037ce0d5791d9
Alkar-stud <alexandre.kapps@gmail.com>
2025-04-16 17:47:17 +0200

    Add grade to user data in SecurityController response

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -141,6 +141,7 @@ final class SecurityController extends AbstractController
                 'pseudo'  => $user->getPseudo(),
                 'photo' => $user->getPhoto(),
                 'credits' => $user->getCredits(),
+                'grade' => $user->getGrade(),
                 'isDriver' => $user->IsDriver(),
                 'isPassenger' => $user->IsPassenger(),
                 'apiToken' => $user->getApiToken(),
