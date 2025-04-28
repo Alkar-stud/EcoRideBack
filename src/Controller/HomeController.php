9e53d6b70d05dabdac9524b32d29288dab9fa8c0
Alkar-stud <alexandre.kapps@gmail.com>
2025-04-28 16:57:06 +0200

    add HomeController with routes for home and API documentation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return new Response('L\'API est <a href="/api/">ici</a>', Response::HTTP_OK);
+    }
+
+    #[Route('/api', name: 'api_app_home')]
+    public function indexApi(): Response
+    {
+        return new Response('La doc de l\'API est <a href="/api/doc/">ici</a>', Response::HTTP_OK);
+    }
+}
