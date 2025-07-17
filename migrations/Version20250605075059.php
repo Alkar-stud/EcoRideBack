a1dfb593c0429c74387a15351a50a2e964cee209
Alkar-stud <alexandre.kapps@gmail.com>
2025-07-17 15:46:37 +0200

    Update default user seed data to use consistent pseudo values for driver and both roles

--- migrations/Version20250605075059.php
+++ migrations/Version20250605075059.php
@@ -17,9 +17,9 @@ final class Version20250605075059 extends AbstractMigration
         $this->addSql(<<<'SQL'
             INSERT INTO `user` (`email`, `roles`, `password`, `pseudo`, `photo`, `credits`, `grade`, `is_driver`, `is_passenger`, `api_token`, `is_active`, `created_at`) VALUES
             ('ecoridestud+admin@alwaysdata.net', '["admin"]', '$2y$13$r.iH55Y3TpA3MJKo7DeMpu0n1h1nYEBsBltwWuQTHR1r9rN/.btUS', 'Admin', NULL, 0, NULL, 0, 0, '86135ae9-54e8-46a7-997c-f3dd345d8b5d48cada49fd53e8b20070', 1, NOW()),
-            ('ecoridestud+driver@alwaysdata.net', '[]', '$2y$13$lxRdn4YV7pmIwSxBXR705usHQkysPzDMOuucjsxHd2vEwGHJVGVUW', 'User1', '7ea468d5fff6a516dc71a2179b04be4a.jpg', 20, NULL, 1, 0, '712a5fbf6e0d20f9ded3d16e43e75ca327fd27d72b29d9f79331dea5505c6880', 1, NOW()),
+            ('ecoridestud+driver@alwaysdata.net', '[]', '$2y$13$lxRdn4YV7pmIwSxBXR705usHQkysPzDMOuucjsxHd2vEwGHJVGVUW', 'Driver', '7ea468d5fff6a516dc71a2179b04be4a.jpg', 20, NULL, 1, 0, '712a5fbf6e0d20f9ded3d16e43e75ca327fd27d72b29d9f79331dea5505c6880', 1, NOW()),
             ('ecoridestud+passenger@alwaysdata.net', '[]', '$2y$13$A7mN8xmw7/JebrAq8wDPkOrj5JUzu0wmnYpt9gB85Gqj1fIt0wqy6', 'Passager', 'f081df8bcbfe5fb2df5e08aceb6f93a4.jpg', 176, NULL, 0, 1, 'b68c64124987c8a459212956185890fe6c4014c610b72e64c2e4dddc7df6818d', 1, NOW()),
-            ('ecoridestud+both@alwaysdata.net', '[]', '$2y$13$HnS0kRmcgppbU8zIm8tY8evDBqg9KUI/aJeaZNdqNk1StJbM2qOy6', 'LesDeux', '6f1eb80a4ef6709098202604a28ebfbc.jpg', 173, NULL, 1, 1, '20b70d4134a344ef418beed6927d393536af3f9c58f1d3de5f4ebdf2e12656c5', 1, NOW()),
+            ('ecoridestud+both@alwaysdata.net', '[]', '$2y$13$HnS0kRmcgppbU8zIm8tY8evDBqg9KUI/aJeaZNdqNk1StJbM2qOy6', 'Both', '6f1eb80a4ef6709098202604a28ebfbc.jpg', 173, NULL, 1, 1, '20b70d4134a344ef418beed6927d393536af3f9c58f1d3de5f4ebdf2e12656c5', 1, NOW()),
             ('employe@email.com', '["employee"]', '$2y$13$LjIjgKZHZz5ACwxW0coxTe/rA8O.NY.zhLdXjlmD5aU7jZquGVCF2', 'Employé', NULL, 0, NULL, 0, 0, '6ba2e9eca47fa6501d49f3b9e1e7fddc647ddd68dd94842f74cd259b15890818', 1, NOW());
         SQL);
 
